582c3ed8bd0a460f601c7903c16e0423f4505279
fihra <fabrofabian@gmail.com>
2024-04-27 12:01:19 -1000

    add tempo provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import DDR_arrows from './assets/DDR_Arrows.png';
 import './App.css'
-import MainTempo from './components/MainTempo';
+import Tempo from './components/tempo/Tempo';
+import { TempoProvider } from './context/TempoContext';
 
 const App = () => {
   return (
@@ -9,9 +10,11 @@ const App = () => {
         <img className="ddr-logo" src={DDR_arrows} alt="DDR Arrows"/>
       </div>
       <h1>DDR Tempo</h1>
-      <div className="card">
-        <MainTempo/>
-      </div>
+      <TempoProvider>
+        <div>
+          <Tempo/>
+        </div>
+      </TempoProvider>
     </>
   )
 }
