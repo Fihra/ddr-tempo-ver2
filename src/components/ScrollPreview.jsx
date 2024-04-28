669555960dfe569f22d4c12a71b6f41259af3534
fihra <fabrofabian@gmail.com>
2024-04-28 00:01:57 -1000

    created scrollPreview component

---
+++ src/components/ScrollPreview.jsx
@@ -0,0 +1,73 @@
+import React, {useState, useContext} from "react";
+import { useSpring, animated } from '@react-spring/web';
+import { TempoContext } from "../context/TempoContext";
+
+const ScrollPreview = () => {
+    const tempoContext = useContext(TempoContext);
+    const { minimumTempo, maximumTempo, currentTempoMod, scrollSpeedPreview } = tempoContext;
+    const [ isScrollPreviewOn, setIsScrollPreviewOn ] = useState(false);
+
+    const minArrowNote = ((60000 / minimumTempo) * 4) / scrollSpeedPreview;
+    const maxArrowNote = ((60000 / maximumTempo) * 4) / scrollSpeedPreview;
+
+    const minSprings = useSpring({
+        from: { y: 320, x: 80 },
+        to: { y: 0 },
+        loop: true,
+        config: {
+            duration: minArrowNote,
+        }
+    })
+
+    const maxSprings = useSpring({
+        from: { y: 320, x: 80 },
+        to: { y: 0 },
+        loop: true,
+        config: {
+            duration: maxArrowNote,
+        }   
+    })
+
+    const showScrollPreview = () => {
+        return(
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+                <div style={{flex: 2}}>
+                    <h5>Min. Tempo</h5>
+                    <animated.div
+                        style={{
+                            width: 50,
+                            height: 50,
+                            background: '#008503',
+                            borderRadius: 8,
+                            ...minSprings,
+                        }}        
+                    />
+                </div>
+                <div>
+                    <h5>| x{scrollSpeedPreview} |</h5>
+                </div>
+                <div style={{flex: 2}}>
+                    <h5>Max. Tempo</h5>
+                    <animated.div
+                        style={{
+                            width: 50,
+                            height: 50,
+                            background: '#D1005B',
+                            borderRadius: 8,
+                            ...maxSprings,
+                        }}
+                    />
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <section className="scroll-preview-container">
+            <button onClick={() => setIsScrollPreviewOn(!isScrollPreviewOn)}>{!isScrollPreviewOn ? "Show Scroll Preview" : "Hide Scroll Preview"}</button>
+            {!isScrollPreviewOn ? null : showScrollPreview()}
+        </section>
+    )
+}
+
+export default ScrollPreview;
