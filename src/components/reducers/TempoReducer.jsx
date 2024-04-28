e1b03937d9961ef9d8223c44773b5d9c19169bd3
fihra <fabrofabian@gmail.com>
2024-04-27 19:28:42 -1000

    restructure tempo state

--- src/components/reducers/TempoReducer.jsx
+++ src/components/reducers/TempoReducer.jsx
@@ -1,25 +1,26 @@
 import TEMPOACTIONS from "../actions/TempoActions";
 
 export const initialState = {
-    tempos: [10, 1000]
+    minimumTempo: 10,
+    maximumTempo: 1000
 }
 
 const TempoReducer = (state, action) => {
     const {type, payload} = action;
-    console.log("Payload: ", payload.minimumTempo)
+    
     switch(type) {
         case TEMPOACTIONS.SET_MIN_TEMPO:
-            // console.log(tempos);
             return {
                 ...state,
-                tempos: payload.minimumTempo
-                
+                minimumTempo: payload.minimumTempo
             }
         case TEMPOACTIONS.SET_MAX_TEMPO:
             return {
                 ...state,
-                tempos: payload.maximumTempo
+                maximumTempo: payload.maximumTempo
             }
+        default:
+            return state;
     }
 }
 
