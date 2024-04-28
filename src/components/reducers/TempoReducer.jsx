a1c8c3e956dd48c65dd22e036a35f0566e6a725d
fihra <fabrofabian@gmail.com>
2024-04-27 22:25:14 -1000

    add tempo mod and speed mod map reducers

--- src/components/reducers/TempoReducer.jsx
+++ src/components/reducers/TempoReducer.jsx
@@ -2,7 +2,20 @@ import TEMPOACTIONS from "../actions/TempoActions";
 
 export const initialState = {
     minimumTempo: 10,
-    maximumTempo: 1000
+    maximumTempo: 1000,
+    currentTempoMod: 1,
+    speedModMap: {
+        0: false,
+        1: false,
+        2: false,
+        3: false,
+        4: false,
+        5: false,
+        6: false,
+        7: false,
+        8: false,
+        9: false,
+    }
 }
 
 const TempoReducer = (state, action) => {
@@ -18,6 +31,16 @@ const TempoReducer = (state, action) => {
             return {
                 ...state,
                 maximumTempo: payload.maximumTempo
+            }
+        case TEMPOACTIONS.SET_CURRENT_TEMPO_MOD:
+            return {
+                ...state,
+                currentTempoMod: payload.currentTempoMod
+            }
+        case TEMPOACTIONS.SET_SPEED_MOD_MAP:
+            return {
+                ...state,
+                speedModMap: payload.speedModMap
             }
         default:
             return state;
