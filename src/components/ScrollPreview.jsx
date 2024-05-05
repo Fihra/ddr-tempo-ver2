cbd69c3c6b581e6afcefe424d0fe78c2d606c3d0
fihra <fabrofabian@gmail.com>
2024-05-04 21:02:25 -1000

    check if mobile screen or desktop for arrow preview

--- src/components/ScrollPreview.jsx
+++ src/components/ScrollPreview.jsx
@@ -10,8 +10,12 @@ const ScrollPreview = () => {
     const minArrowNote = ((60000 / minimumTempo) * 4) / scrollSpeedPreview;
     const maxArrowNote = ((60000 / maximumTempo) * 4) / scrollSpeedPreview;
 
+    const checkWindowSize = () => {
+        return window.innerWidth > 326 ? 100 : 30
+    }
+
     const minSprings = useSpring({
-        from: { y: 320, x: 80 },
+        from: { y: 320, x: checkWindowSize() },
         to: { y: 0 },
         loop: true,
         config: {
@@ -20,7 +24,7 @@ const ScrollPreview = () => {
     })
 
     const maxSprings = useSpring({
-        from: { y: 320, x: 80 },
+        from: { y: 320, x: checkWindowSize() },
         to: { y: 0 },
         loop: true,
         config: {
@@ -33,6 +37,7 @@ const ScrollPreview = () => {
             <div style={{display: 'flex', flexDirection: 'row'}}>
                 <div style={{flex: 2}}>
                     <h5>Min. Tempo</h5>
+                    <div>
                     <animated.div
                         style={{
                             width: 50,
@@ -42,12 +47,15 @@ const ScrollPreview = () => {
                             ...minSprings,
                         }}        
                     />
+                    </div>
                 </div>
                 <div>
-                    <h5>| x{scrollSpeedPreview} |</h5>
+                    <h5>|x{scrollSpeedPreview} |</h5>
+                    <h5>({minimumTempo} - {maximumTempo})</h5>
                 </div>
                 <div style={{flex: 2}}>
                     <h5>Max. Tempo</h5>
+                    <div>
                     <animated.div
                         style={{
                             width: 50,
@@ -57,6 +65,7 @@ const ScrollPreview = () => {
                             ...maxSprings,
                         }}
                     />
+                    </div>
                 </div>
             </div>
         )
